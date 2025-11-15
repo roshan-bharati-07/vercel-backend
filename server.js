d8b01de2ef6feb707b45972b0b9c8c25c3af1d9d
roshan-bharati-07 <bharati.roshan.098@gmail.com>
2025-11-15 16:52:14 +0545

    listen is not required in vercel

--- server.js
+++ server.js
@@ -16,5 +16,4 @@ mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+export default app;
